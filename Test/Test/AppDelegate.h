94398be13e3da649515258b911027c95c9b1f747
Mike Brantley <mjbrantley00@gmail.com>
2014-05-11 09:40:14 -0700

    Initial upload
    Initial upload

---
+++ Test/Test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by Mike Brantley on 5/11/14.
+//  Copyright (c) 2014 Mike Brantley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
